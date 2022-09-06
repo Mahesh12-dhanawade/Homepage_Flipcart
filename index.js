0d7de8c73b93d30de43409c5b31f8ce536489619
maheshvdhanawade <maheshgitt9324@gmail.com>
2022-09-06 16:40:12 +0530

    dark-mode commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function changeMode(){
+    let myBody = document.body;
+    // myBody.classList.add('myDark');
+    document.getElementsByClassName('fa-moon')[0].style.display="block";
+    myBody.classList.toggle('myDark')
+}
+
+
+
+
+
+
+
+
+
+
+
