4594c7e78689c91318bf5252ddae3ad6cdcd48c5
maheshvdhanawade <maheshgitt9324@gmail.com>
2022-09-15 19:22:48 +0530

    coupen-code

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function changeMode(){
-    let myBody = document.body;
-    // myBody.classList.add('myDark');
-    document.getElementsByClassName('fa-moon')[0].style.display="block";
-    myBody.classList.toggle('myDark')
+function changeMode() {
+  let myBody = document.body;
+  // myBody.classList.add('myDark');
+  document.getElementsByClassName("fa-moon")[0].style.display = "block";
+  myBody.classList.toggle("myDark");
 }
 
+function loadCoupon() {
+  document.getElementById("coupon").style.display = "block";
+  document.getElementById("header").style.opacity = "0.5";
+}
 
-
-
-
-
-
-
-
-
-
+const closeCoupon = () => {
+  document.getElementById("coupon").style.display = "none";
+  document.getElementById("header").style.opacity = "1";
+};
